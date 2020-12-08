37dfc4cd4fa6f123a8199a0e409c2f6ea21697d9
bogialmasi <71004475+bogialmasi@users.noreply.github.com>
2020-12-08 12:42:01 +0100

    osszetettfeladat1203 kijavítva + output

--- Osszetettfeladat1203.java
+++ Osszetettfeladat1203.java
@@ -16,9 +16,9 @@ public class Osszetettfeladat1203 {
     public static void main(String[] args) {
         System.out.println("\n ÖSSZETETT FELADATSOR\nAB 113C");
         Scanner bemenet = new Scanner(System.in);
-        char valasz = 0;
+        char valasz;
         do{
-            int darabszam = (int) (Math.random() * 10) + 30;
+            int darabszam = (int) (Math.random() * 10 + 30);
         
         int szamok1[] = new int[darabszam];
         int szamok2[] = new int[darabszam];
@@ -28,56 +28,51 @@ public class Osszetettfeladat1203 {
             szamok2[i] = (int) (Math.random() * -50) + 50;
         }
         System.out.println("\nA szamok1 tömb elemei: ");
-        for (int i = 0; i < darabszam; i++) {
+        for (int i = 0; i < szamok1.length; i++) {
             System.out.print(String.format("%,4d", szamok1[i]) + " ");
         }
         System.out.println("\nA szamok2 tömb elemei: ");
-        for (int i = 0; i < darabszam; i++) {
+        for (int i = 0; i < szamok2.length; i++) {
             System.out.print(String.format("%,4d", szamok2[i]) + " ");
         }
-
         int ujTomb[] = new int[darabszam];
-        int ujTombdb = 0;
         for (int i = 0; i < darabszam; i++) {
             if (szamok1[i] % 5 == 0) {
-                ujTomb[ujTombdb++] = szamok1[i];
+                ujTomb[i] = szamok1[i];
             }
-            if (szamok2[i] % 5 == 0) {
-                ujTomb[ujTombdb++] = szamok2[i];
-            }
-
+        for (int j = 0; j < darabszam; j++) {
+            if (szamok2[j] % 5 == 0){
+                ujTomb[j+1] = szamok2[j];
+            }    
+        }    
         }
         System.out.println("\nAz ujTomb elemei: ");
-        for (int i = 0; i < ujTombdb; i++) {
+        for (int i = 0; i < ujTomb.length; i++) {
             System.out.print(String.format("%,4d", ujTomb[i]) + " ");
         }
         
-        for (int i = 0; i < ujTombdb; i++) {
-            if (ujTomb[i] == 0) {
-                System.out.println("Az ujTomb 0-t tartalmaz");
+        int legkisebb = ujTomb[0];
+        int legnagyobb = ujTomb[0];
+            for (int i = 1; i < ujTomb.length; i++) { 
+                if (ujTomb[i] > legnagyobb) {
+                    legnagyobb = ujTomb[i];
+                }
             }
-        }
-        int legnagyobb = szamok1[0];
-        int legkisebb = szamok1[0];
-        for (int i = 0; i < darabszam; i++) {
-            if (ujTomb[i] > legnagyobb) {
-                legnagyobb = szamok1[i];
-            }
-        }
-        for (int i = 0; i < darabszam; i++) {
+        for (int i = 0; i < ujTomb.length; i++) {
             if (ujTomb[i] < legkisebb) {
-                legkisebb = szamok1[i];
+                legkisebb = ujTomb[i];
             }
         }
         
             System.out.println("\nA legnagyobb elem: " + legnagyobb + ", a legkisebb elem: " + legkisebb);
+
+        for (int i = 0; i < ujTomb.length; i++) {
+            if (ujTomb[i] == 0) {
+                System.out.println("Az ujTomb 0-t tartalmaz");
+                break;
+            }
+        }
         
-        if (legnagyobb % legkisebb == 0) {
-            System.out.println("\nAz ujTomb tömb legnagyobb eleme a legkisebb többszöröse");
-        }
-        else {
-            System.out.println("\nA ujTomb tömb legnagyobb eleme nem többszöröse a legkisebbnek");
-        }
         System.out.println("Újra lefuttatja a programot? i/n: ");
         valasz = bemenet.next().charAt(0);
     } while ((valasz == 'i') || (valasz == 'I'));

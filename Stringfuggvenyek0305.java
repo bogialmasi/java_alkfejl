e3286409829bc26c21da35d433f89addc2611b41
bogialmasi <71004475+bogialmasi@users.noreply.github.com>
2021-03-08 10:49:16 +0100

    string függvények folytatva

--- Stringfuggvenyek0305.java
+++ Stringfuggvenyek0305.java
@@ -6,6 +6,7 @@
 package stringfuggvenyek0305;
 
 //import java.nio.charset.StandardCharsets;
+import java.util.Locale;
 import java.util.Scanner;
 
 /**
@@ -29,27 +30,33 @@ public class Stringfuggvenyek0305 {
         String kNev = sc.nextLine();
         sc.close();
         
+        
+        String teljes = tNev + " " + kNev;
+        
+        int szokozindex1 = teljes.indexOf(' ') +1;
+        int szokozindex2 = teljes.lastIndexOf(' ') +1;
+        
+        String[] parts = tNev.split(" ");
+        String tvNev = parts[0];
+        String tkNev = parts[1];
+        char tvMono = tvNev.charAt(0);
+        char tkMono = tkNev.charAt(0);
+        
         System.out.println("Nagybetűs: " + tNev.toUpperCase());
         System.out.println("Szókezdők nagybetűsek: ");
         System.out.println("Kisbetűs: " + tNev.toLowerCase());
         System.out.println("Karakterszám: " + tNev.length());
-        int szokozindex1 = tNev.indexOf(' ');
         System.out.println("A névben az 1.  szóköz helye: " + szokozindex1);
-        int szokozindex2 = tNev.lastIndexOf(' ');
         System.out.println("A névben a 2. szóköz helye:" + szokozindex2);
-        
-        String[] parts = tNev.split(" ");
-        String kvNev = parts[0];
-        String kkNev = parts[1];
-        System.out.println("1. keresztnév kezdő pozíciója:");
-        System.out.println("2. keresztnév kezdő pozíciója:");
-        System.out.println("Vezetéknév: " + kvNev);
-        System.out.println("Keresztnév: " + kkNev);
-        System.out.println("Vezetéknév hossza: " + kvNev.length());
-        System.out.println("Keresztnév hossza: " + kkNev.length());
-        System.out.println("Monogram: ");
-        System.out.println("Névegyesítés (vezetéknév + keresztnév): ");
-        System.out.println("Új név: ");
+        System.out.println("1. keresztnév kezdő pozíciója:" + (teljes.indexOf(tkNev) +1) );
+        System.out.println("2. keresztnév kezdő pozíciója:" + (teljes.indexOf(kNev) +1) );
+        System.out.println("Vezetéknév: " + tvNev);
+        System.out.println("Keresztnév: " + tkNev);
+        System.out.println("Vezetéknév hossza: " + tvNev.length());
+        System.out.println("Keresztnév hossza: " + tkNev.length());
+        System.out.println("Monogram: " + tvMono + " " + tkMono);
+        System.out.println("Névegyesítés (vezetéknév + keresztnév): " + tvNev + tkNev );
+        System.out.println("Új név: " + tNev.replace(tkNev, kNev));
     }
     
 }
